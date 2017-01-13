f127ed1579862c8d2a13c892b5a92d43b2c8b8fd
Michelle Lee <lee.michellejung@gmail.com>
2017-01-13 15:14:32 -0800

    copies everything from lecture

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ var app = express();
 app.use( express.static(__dirname + '/public' ));
 app.set('view engine', 'ejs'); // res.render(file) => '/views/file'
 
+app.get('/list', function(req, res){
+  var locals = {
+    message: 'Hello from Views',
+    list:list
+  };
+  res.render('list', locals);
+});
+
+// define a route that returns JSON of list info
+// GET '/list.json' => ['Mulder', 'Scully'];
+var list = ['Mulder', 'Scully'];
+
+app.get('/list.json', function(req, res){
+  res.json(list);
+})
 
 var port = 3000;
 app.listen(port, function(){
